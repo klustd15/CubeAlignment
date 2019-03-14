850a8f5b37b1041846cdcf37349fa372f36a0002
klustd15 <klustd15@htl-kaindorf.ac.at>
2019-03-14 12:04:45 +0100

    adding whole project

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectSpawner : MonoBehaviour {
+    private int cnt;
+    public GameObject Prefab;
+    // Use this for initialization
+	void Start () {
+        cnt = 0;
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            /* LEGACY CODE
+               GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+               cube.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 10;
+               Material mat = null;
+               if (cnt % 2 == 0)
+                   mat= Resources.Load("cube1") as Material;
+               else
+                   mat = Resources.Load("cube2") as Material;
+               cnt++;
+               cube.GetComponent<Renderer>().material = mat;
+               cube.tag = "cube";*/
+
+
+            GameObject cube = Instantiate(Prefab) as GameObject;
+            cube.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 10;
+            /*Material mat = null;
+            if (cnt % 2 == 0)
+                mat = Resources.Load("cube1") as Material;
+            else
+                mat = Resources.Load("cube2") as Material;
+            cnt++;
+            cube.GetComponent<Renderer>().material = mat;*/
+            cube.GetComponent<Renderer>().enabled = true;
+            cube.tag = "cube";
+        } 
+    }
+
+
+}
---
+++ Assets/Scripts/SnapCubes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapCubes : MonoBehaviour {
+    List<GameObject> gos;
+
+    // Use this for initialization
+    void Start () {
+        gos = new List<GameObject>(GameObject.FindGameObjectsWithTag("cube"));
+        gos.Remove(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject go = GetClosestCube(gos);
+        float distance = Vector3.Distance(go.transform.position, transform.position);
+        if (distance < 1.3f)
+            fixYAxisAndRotation(go);
+        gos = new List<GameObject>(GameObject.FindGameObjectsWithTag("cube"));
+        gos.Remove(gameObject);
+    }
+
+    void fixYAxisAndRotation(GameObject closestGameObject)
+    {
+        gameObject.transform.SetPositionAndRotation(
+        new Vector3(gameObject.transform.position.x, closestGameObject.transform.position.y, gameObject.transform.position.z),
+        closestGameObject.transform.rotation);
+
+        print("fixed y-axis");
+    }
+    
+    GameObject GetClosestCube(List<GameObject> cubes)
+    {
+
+        GameObject cMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject c in cubes)
+        {
+            float dist = Vector3.Distance(c.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                cMin = c;
+                minDist = dist;
+            }
+        }
+        return cMin;
+    }
+}
